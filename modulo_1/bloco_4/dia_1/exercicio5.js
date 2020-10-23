eb9d2b10e6fefd5a4017d4f0e505e46580d7dbbf
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-10-23 18:03:20 -0300

    adicionando exercicio 5

---
+++ modulo_1/bloco_4/dia_1/exercicio5.js
@@ -0,0 +1,11 @@
+let a = 30;
+let b = 60;
+let c = 90;
+
+result = a + b + c;
+
+if(result == 180){
+    console.log("true");
+} else {
+    console.log("Esses ângulos não formam um triângulo!");
+}
