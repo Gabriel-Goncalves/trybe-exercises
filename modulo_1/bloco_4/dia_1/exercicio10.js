1e93cdddb352302d1f51686a47ccc4300554de20
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-10-23 18:44:24 -0300

    adicionando exercicio 10

---
+++ modulo_1/bloco_4/dia_1/exercicio10.js
@@ -0,0 +1,11 @@
+let valor = 1000;
+let valorVenda = 1550;
+
+if (valor < 0 || valorVenda < 0) {
+  console.log("Os valores não podem ser menor que 0");
+} else {
+  let valorCustoTotal = 1000 * 1.2;
+  let lucro = valorVenda - valorCustoTotal;
+  let lucroTotal = lucro * 1000;
+  console.log("Lucro total = R$ " + lucroTotal);
+}
