7c9a2e81329d9cbba19c73ef69d840e1ac22dfb4
Gabriel Gonçalves <gabriel_98gm@hotmail.com>
2020-10-23 20:27:41 -0300

    ajustando exercicio 9 e 10

--- modulo_1/bloco_4/dia_1/exercicio10.js
+++ modulo_1/bloco_4/dia_1/exercicio10.js
@@ -4,7 +4,7 @@ let valorVenda = 1550;
 if (valor < 0 || valorVenda < 0) {
   console.log("Os valores não podem ser menor que 0");
 } else {
-  let valorCustoTotal = 1000 * 1.2;
+  let valorCustoTotal = valor * 1.2;
   let lucro = valorVenda - valorCustoTotal;
   let lucroTotal = lucro * 1000;
   console.log("Lucro total = R$ " + lucroTotal);
--- modulo_1/bloco_4/dia_1/exercicio9.js
+++ modulo_1/bloco_4/dia_1/exercicio9.js
@@ -5,7 +5,7 @@ let c = 9;
 let mod = (a % 2) + (b % 2) + (c % 2);
 
 if (mod === 0) {
-  console.log("True");
-} else {
   console.log("False");
+} else {
+  console.log("True");
 }
