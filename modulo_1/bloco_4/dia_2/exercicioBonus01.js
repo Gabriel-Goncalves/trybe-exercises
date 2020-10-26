2e8e4dbbc85f95e1b184ea13e44e7ca74722d072
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-10-26 15:47:23 -0300

    adicionando exercicio bonus 1

---
+++ modulo_1/bloco_4/dia_2/exercicioBonus01.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let auxiliar;
+
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = i; j < numbers.length; j++) {
+    if (numbers[j] < numbers[i]) {
+      auxiliar = numbers[i];
+      numbers[i] = numbers[j];
+      numbers[j] = auxiliar;
+    }
+  }
+}
+
+console.log(numbers);
