f27227e1da37639c38d4f58fe9c9182d71e29c0b
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-10-27 16:34:55 -0300

    adicionando exercicio 6

---
+++ modulo_1/bloco_4/dia_3/exercicio06.js
@@ -0,0 +1,15 @@
+n = 17;
+let resultado = 'é primo';
+
+if(n == 2){
+    console.log(`${n} é primo`)
+} else{
+    for (let count = 2; count < n; count++) {
+        if(n%count == 0){
+            resultado = "não é primo";
+            count = n;
+        }
+    }
+    console.log(`${n} ${resultado}`);
+}
+
