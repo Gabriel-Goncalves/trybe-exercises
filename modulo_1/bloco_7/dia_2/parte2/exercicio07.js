8053f2416a37b4f6e659bd062f5b99d090f4e60e
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-11-24 18:18:41 -0300

    adicionando exercicio 07 parte 2

---
+++ modulo_1/bloco_7/dia_2/parte2/exercicio07.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: "Matemática",
+  numeroEstudantes: 20,
+  professor: "Maria Clara",
+  turno: "manhã",
+};
+
+const lesson2 = {
+  materia: "História",
+  numeroEstudantes: 20,
+  professor: "Carlos",
+};
+
+const lesson3 = {
+  materia: "Matemática",
+  numeroEstudantes: 10,
+  professor: "Maria Clara",
+  turno: "noite",
+};
+
+const getValueByNumber = (obj, position) => {
+    const values = Object.values(obj);
+    return values[position];
+}
+
+console.log(getValueByNumber(lesson3, 0));
