cd27c42c7a4a1becfb5a249be2f119533fa83467
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-12-03 17:49:09 -0300

    melhorando exercicio 2

--- modulo_1/bloco_8/dia_4/exercicio02.js
+++ modulo_1/bloco_8/dia_4/exercicio02.js
@@ -1,15 +1,15 @@
-const assert = require('assert')
+const assert = require("assert");
 
 const sum = (...args) => {
-    let sum = 0;
-    for (let index = 0; index < args.length; index += 1) {
-        sum += args[index];
-    }
-    return sum;
-}
+  const sum = args.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+  );
+  return sum;
+};
 
-assert.strictEqual(sum(), 0)
-assert.strictEqual(sum(1), 1)
-assert.strictEqual(sum(1, 2), 3)
-assert.strictEqual(sum(1, 2, 3), 6)
-assert.strictEqual(sum(1, 2, 3, 4), 10)
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
