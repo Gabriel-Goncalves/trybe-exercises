dd0a26448eb4d48732dea043835764da4bb15e83
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-03-11 09:33:23 -0300

    criando componente father

---
+++ modulo_2/bloco_18/dia_2/src/Components/Father.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Daughter from './Daughter';
+
+const Father = () => {
+  return (
+    <div>
+      <h1>Father</h1>
+      <Daughter />
+    </div>
+  )
+}
+
+export default Father;
--- modulo_2/bloco_18/dia_2/src/Components/GreatGrandMother.jsx
+++ modulo_2/bloco_18/dia_2/src/Components/GreatGrandMother.jsx
@@ -4,6 +4,7 @@ import GrandMother from "./GrandMother";
 
 function GreatGrandMother() {
   const [money, spend] = useState(100000);
+
   return (
     <MyContext.Provider value={money}>
       <h1>Great Grand Mother</h1>
