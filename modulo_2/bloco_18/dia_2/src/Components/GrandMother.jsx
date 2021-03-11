e54af5b9e538caf15455614a98cf8bb2f9670b60
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-03-11 09:29:24 -0300

    adicionando componentes filho

---
+++ modulo_2/bloco_18/dia_2/src/Components/GrandMother.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Father from "react";
+
+export default function GrandMother() {
+  return (
+    <div>
+      <h1>GrandMother</h1>
+      <Father />
+    </div>
+  );
+}
