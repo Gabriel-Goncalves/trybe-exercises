6b9e00931e125c51986700db7602091c411edbdf
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-04-08 18:33:20 -0300

    finalizando modulo 2

--- modulo_2/bloco_18/redux-with-hooks/src/components/ButtonDown.jsx
+++ modulo_2/bloco_18/redux-with-hooks/src/components/ButtonDown.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import {counDown} from '../actions';
+import { counDown } from "../actions";
 
 const ButtonDown = () => {
   const dispatch = useDispatch();
