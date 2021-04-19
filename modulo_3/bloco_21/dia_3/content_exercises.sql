678472cc5ef3380123c726e8eb675b875104e211
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-04-19 18:54:22 -0300

    dia 3 bloco 21

---
+++ modulo_3/bloco_21/dia_3/content_exercises.sql
@@ -0,0 +1,132 @@
+USE sakila;
+DELIMITER $$
+
+CREATE PROCEDURE ShowAllActors()
+BEGIN
+    SELECT * FROM sakila.actor;
+END $$
+
+DELIMITER ;
+
+CALL ShowAllActors;
+
+
+-- ------------------
+
+
+USE sakila;
+DELIMITER $$
+
+CREATE FUNCTION MoviesWithActor(actor_id int)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE movie_total INT;
+    SELECT COUNT(*)
+    FROM sakila.film_actor
+    WHERE sakila.film_actor.actor_id = actor_id INTO movie_total;
+    RETURN movie_total;
+END $$
+
+DELIMITER ;
+
+SELECT MoviesWithActor(1);
+
+
+
+SELECT
+    address,
+    district,
+    (
+        SELECT city
+        FROM sakila.city
+        WHERE city.city_id = sakila.address.city_id
+    ) AS city
+FROM sakila.address;
+
+CREATE DATABASE IF NOT EXISTS rede_social;
+
+USE rede_social;
+
+CREATE TABLE perfil(
+    perfil_id INT PRIMARY KEY auto_increment,
+    saldo DECIMAL(10, 2) NOT NULL DEFAULT 0,
+    ultima_atualizacao DATETIME,
+    acao VARCHAR(50),
+    ativo BOOLEAN DEFAULT 1
+) engine = InnoDB;
+
+CREATE TABLE log_perfil(
+    acao_id INT PRIMARY KEY AUTO_INCREMENT,
+    acao VARCHAR(300),
+    data_acao DATE
+) engine = InnoDB;
+
+
+USE rede_social;
+
+DELIMITER $$
+CREATE TRIGGER trigger_perfil_insert
+    BEFORE INSERT ON perfil
+    FOR EACH ROW
+BEGIN
+    SET NEW.ultima_atualizacao = NOW(),
+        NEW.acao = 'INSERT';
+END $$
+DELIMITER ;
+
+
+USE rede_social;
+
+DELIMITER $$
+CREATE TRIGGER trigger_perfil_update
+    BEFORE UPDATE ON perfil
+    FOR EACH ROW
+BEGIN
+    SET NEW.ultima_atualizacao = NOW(),
+        NEW.acao = 'UPDATE';
+END $$
+DELIMITER ;
+
+
+
+UPDATE perfil
+SET saldo = 3000
+WHERE perfil_id = 1;
+
+SELECT * FROM perfil;
+
+
+USE rede_social;
+
+DELIMITER $$
+CREATE TRIGGER trigger_perfil_delete
+    AFTER DELETE ON perfil
+    FOR EACH ROW
+BEGIN
+    INSERT INTO log_perfil(acao, data_acao)
+    VALUES ('exclusão', NOW());
+END $$
+DELIMITER ;
+
+
+
+USE betrybe_automoveis;
+
+CREATE TABLE carros(
+    id_carro INT PRIMARY KEY auto_increment,
+    preco DECIMAL(12, 2) NOT NULL DEFAULT 0,
+    data_atualizacao DATETIME,
+    acao VARCHAR(15),
+    disponivel_em_estoque BOOLEAN DEFAULT 0
+) engine = InnoDB;
+
+CREATE TABLE log_operacoes(
+    operacao_id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_operacao VARCHAR(15) NOT NULL,
+    data_ocorrido DATE NOT NULL
+) engine = InnoDB;
+
+
+
+
+
